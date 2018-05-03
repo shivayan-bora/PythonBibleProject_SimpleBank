124c75eeeca8ea6c50bd5739727b1270c10b13a8
Shivayan Bora <bora.shivayan@gmail.com>
2018-05-04 01:34:30 +0530

    First Commit

---
+++ coins.py
@@ -0,0 +1,13 @@
+class Pound:
+    value = 1.00
+    colour = "Gold"
+    num_edges = 1
+    diameter = 22.5 #mm
+    thickness = 3.15 #mm
+    heads = True
+
+
+coin1 = Pound()
+
+print(type(coin1))
+print(coin1.value)
